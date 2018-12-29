0e3c3915f7ee8a0b96b2d783376bd3a10ed11dde
Mike de Klerk <mikedeklerk@gmail.com>
2018-12-29 11:55:29 +0100

    Adds 1-bit less than comparer.

---
+++ LogicCircuit.Test/Alu/CompareTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LogicCircuit.Alu.Compare;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using bool3 = System.Tuple<bool, bool, bool>;
+
+namespace LogicCircuit.Test.Alu
+{
+    [TestClass]
+    public class CompareTest
+    {
+        [TestMethod]
+        public void LessThanComparesCorrectly()
+        {
+            //Given
+            var lessThan = new LessThan();
+            var table = new List<bool3>
+            {
+                new bool3(false, false, false),
+                new bool3(false, true, true),
+                new bool3(true, false, false),
+                new bool3(true, true, false),
+            };
+
+            foreach (var t in table)
+            {
+                //When
+                lessThan.InputA.State = t.Item1;
+                lessThan.inputB.State = t.Item2;
+
+                //Then
+                lessThan.Output.State.Should().Be(t.Item3);
+            }
+        }
+    }
+}
---
+++ LogicCircuit/Alu/Compare/LessThan.cs
@@ -0,0 +1,21 @@
+﻿using LogicCircuit.Gates.Simple;
+using LogicCircuit.Infrastructure;
+
+namespace LogicCircuit.Alu.Compare
+{
+    public class LessThan
+    {
+        private readonly NOT not = new NOT();
+        private readonly AND and = new AND();
+
+        public InputPin InputA {  get { return not.InputA; } }
+        public InputPin inputB {  get { return and.InputB; } }
+
+        public OutputPin Output { get { return and.Output; } }
+
+        public LessThan()
+        {
+            not.Output.ConnectTo(and.InputA);
+        }
+    }
+}
